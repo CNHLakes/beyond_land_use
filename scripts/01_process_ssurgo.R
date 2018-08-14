8fdc4c7609b8ed49207e7fbe22f86512d88ea0fa
jsta <stachel2@msu.edu>
2018-08-14 17:48:01 -0400

    towards ssurgo queries

--- scripts/01_process_ssurgo.R
+++ scripts/01_process_ssurgo.R
@@ -1,7 +1,7 @@
 library(raster)
 library(sf)
 
-gpkg_path <- path.expand("~/Documents/Science/Data/gSSURGO/")
+gpkg_path <- path.expand("../gSSURGO/")
 ssurgo_path <- "data/gssurgo/"
 
 source("scripts/utils.R")
@@ -15,6 +15,15 @@ r      <- raster(r_list[grep(llid, r_list)[1]])
 bbox   <- st_bbox(r)
 states <- get_states(bbox)
 
-in_gpkg <- list.files(gpkg_path, pattern = paste0(states, ".gpkg"))
+in_gpkg <- list.files(gpkg_path, pattern = paste0(states, ".gpkg"), 
+                      full.names = TRUE, include.dirs = TRUE)
+
+# con <- src_sqlite(in_gpkg)
+# src_tbls(con)
+qry <- tbl(con, "Valu1") %>%
+  select(mukey, pwsl1pomu) %>%
+  dbplyr::sql_render() %>%
+  stringr::str_replace_all(c("`" = "", "\n" = " "))
+
 
 
