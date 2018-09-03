38132baf3529c9534474a44be40894de247e1e5a
jsta <stachel2@msu.edu>
2018-09-03 12:38:32 -0400

    stop fighting with reticulate, rebuild if query changes

--- scripts/01_process_ssurgo.R
+++ scripts/01_process_ssurgo.R
@@ -7,39 +7,40 @@ library(reticulate)
 use_condaenv("gSSURGO")
 gssurgo <- import("gssurgo")
 
+source("scripts/utils.R")
+ep          <- readRDS("data/ep.rds")
 gpkg_path   <- path.expand("~/Documents/Science/Data/gssurgo_data/gpkgs/")
-aoi_path    <- path.expand("~/Documents/Science/Data/gssurgo_data/aois/")
+aoi_path    <- path.expand("~/Documents/Science/Data/gssurgo_data/aois")
 gssurgo_key <- read.csv("data/gssurgo/gssurgo_key.csv", stringsAsFactors = FALSE)
+
 out_path    <- "data/gssurgo/gssurgo.rds"
 if(file.exists(out_path)){
   res_disk    <- readRDS(out_path)
 }
 
-
-source("scripts/utils.R")
-
-ep <- readRDS("data/ep.rds")
-
-# ---- execute ---- 
 r_list <- list.files(aoi_path, pattern = "^\\d*_\\d.tif$", 
                      include.dirs = TRUE, full.names = TRUE)
 llids  <- stringr::str_extract(r_list, "(\\d*)(?=_\\d.tif)")
 
-pull_metric <- function(col_name, qry, agg_type, r_list, llids, res_disk, 
+pull_metric <- function(col_name, qry, agg_type, r_list, llids, res_disk, gssurgo_key,
                         overwrite = FALSE){
   raw_r_list <- r_list
   raw_llids  <- llids
-  # print(paste("in r_list length", length(r_list)))
   
   if(file.exists(out_path) & !overwrite){
-    if(col_name %in% names(res_disk)){
-      missing_llids <- !(llids %in% res_disk[,!is.na(col_name)][,"llid"])
+    qry_changed <- qry != res_disk$gssurgo_key[
+                            res_disk$gssurgo_key$metric == col_name, 
+                            "query"]
+    # print(paste0(col_name, " query has changed: ", qry_changed))
+    
+    if(col_name %in% names(res_disk$res) & !qry_changed){
+      missing_llids <- !(llids %in% res_disk$res[,!is.na(col_name)][,"llid"])
       llids         <- llids[missing_llids]
       r_list        <- r_list[missing_llids]
     
       if(sum(missing_llids) == 0){
         return(setNames(data.frame(llid = raw_llids, 
-                                 value = res_disk[,col_name]), 
+                                 value = res_disk$res[,col_name]), 
                       c("llid", col_name)))
       }
     }
@@ -56,13 +57,16 @@ pull_metric <- function(col_name, qry, agg_type, r_list, llids, res_disk,
     src_tif       <- r_list[i]
     # print(src_tif)
     base_r        <- suppressMessages(raster(src_tif))
-  
-    # # echo python command
-    # paste0("gssurog.query_gpkg(", src_tif, 
-    #        " ", gpkg_path, 
-    #        " ", qry,
-    #        " ", file.path(aoi_path, "temp.tif"))
-    gssurgo$query_gpkg(src_tif, gpkg_path, qry, file.path(aoi_path, "temp.tif"))
+    
+    qry           <- file.path("/home/jose/Documents/Science/Dissertation/Analysis/_episodes_rmd/lagos_ag/data/gssurgo", paste0(col_name, ".sql"))
+    
+    py_string <- paste0("gssurgo.query_gpkg('", src_tif,
+           "', '", gpkg_path,
+           "', '", qry,
+           "', '", file.path(aoi_path, "temp.tif')"))
+
+    writeLines(c("import gssurgo", py_string), "temp.py")    
+    system("python temp.py")
                        
     data_r           <- suppressMessages(raster(file.path(aoi_path, "temp.tif")))
     # suppressMessages(mapview::mapview(base_r) + mapview::mapview(data_r))
@@ -79,11 +83,9 @@ pull_metric <- function(col_name, qry, agg_type, r_list, llids, res_disk,
              value = unlist(res)), c("llid", col_name))
 }
 
+# ---- execute ---- 
 res <- apply(gssurgo_key, 1, function(x) 
-  pull_metric(x[1], x[2], x[3], r_list, llids, res_disk))
-
-# res <- apply(gssurgo_key[3,], 1, function(x) 
-#   pull_metric(x[1], x[2], x[3], r_list[1:100], llids[1:100], res_disk[1:100,], overwrite = FALSE))
+  pull_metric(x[1], x[2], x[3], r_list, llids, res_disk, gssurgo_key))
 
 res <- dplyr::bind_rows(res) %>% 
   group_by(llid) %>%
@@ -91,4 +93,5 @@ res <- dplyr::bind_rows(res) %>%
   tidyr::fill(wetland_potential:clay_pct) %>%
   na.omit
 
-saveRDS(res, out_path)
+saveRDS(list(res = res, gssurgo_key = gssurgo_key), out_path)
+# res <- readRDS(out_path)
