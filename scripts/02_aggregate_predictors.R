72268f6f8d1847316193f4424238728c7c8dd55a
jsta <stachel2@msu.edu>
2018-09-04 14:35:38 -0400

    create a variable units key

--- scripts/02_aggregate_predictors.R
+++ scripts/02_aggregate_predictors.R
@@ -1,5 +1,5 @@
-library(dplyr)
-library(magrittr)
+suppressMessages(library(dplyr))
+suppressMessages(library(magrittr))
 
 # response variables are medians from 1995 - 2005
 ep <- readRDS("data/ep.rds")
@@ -12,14 +12,33 @@ usgs <- readRDS("data/usgs/usgs.rds") %>%
   select(lagoslakeid, phosphorus_input, nitrogen_input, n_dep)
 
 gssurgo <- ungroup(readRDS("data/gssurgo/gssurgo.rds")$res)
+gssurgo_key <- readRDS("data/gssurgo/gssurgo.rds")$gssurgo_key
 
 cdl <- read.csv("data/cdl/cdl_summary.csv", stringsAsFactors = FALSE)
 
 dt <- ep %>%
-  left_join(lg_lulc) %>%
-  left_join(usgs) %>%
+  left_join(lg_lulc, by = "lagoslakeid") %>%
+  left_join(usgs, by = "lagoslakeid") %>%
   left_join(mutate(gssurgo, llid = as.integer(as.character(llid))), 
             by = c("lagoslakeid" = "llid")) %>%
   left_join(cdl, by = c("lagoslakeid" = "llid"))
 
 saveRDS(dt, "data/dt.rds")
+
+dt_units <- data.frame(variable = names(dt), stringsAsFactors = FALSE) %>%
+  mutate(units = case_when(
+    variable %in% names(cdl)[-1] ~ "percent",
+    variable %in% names(usgs)[-1] ~ "kg/ha",
+    grepl("_ha$", tolower(variable)) ~ "hectares",
+    grepl("_ratio$", tolower(variable)) ~ "",
+    variable %in% names(ep)[c(-1, -2, -3)] ~ "ug/l",
+    grepl("pct$", tolower(variable)) ~ "percent"
+  ))
+
+dt_units <- left_join(dt_units, 
+          dplyr::select(gssurgo_key, metric, units = agg_type), 
+          by = c("variable" = "metric")) %>%
+  mutate(units = coalesce(units.x, units.y)) %>%
+  select(variable, units)
+
+saveRDS(dt_units, "data/dt_units.rds")
