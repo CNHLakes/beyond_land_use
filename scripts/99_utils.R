50cccd10c2230e23e6d46190617ca46c023ec15f
jsta <stachel2@msu.edu>
2019-02-07 17:40:01 -0500

    use smoothr to fill polygon holes

--- scripts/99_utils.R
+++ scripts/99_utils.R
@@ -27,6 +27,7 @@ library(nhdR)
 library(FedData)
 suppressMessages(library(raster))
 library(progress)
+library(smoothr)
 
 theme_opts <- theme(axis.text = element_blank(),
                     axis.ticks = element_blank(),
