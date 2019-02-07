293292ce347c048660e4ecf39ac93137ebdaa1bd
jsta <stachel2@msu.edu>
2019-02-07 10:27:01 -0500

    account for lakes without streams, save plots for debugging

--- scripts/00_get_buffers.R
+++ scripts/00_get_buffers.R
@@ -14,6 +14,7 @@ join_key <- function(vals){
 }
 
 get_buffer_stats <- function(llid){
+  # llid <- 2167
   ll_pnt <- st_coordinates(
     st_transform(
       query_gis("LAGOS_NE_All_Lakes_4ha_POINTS", "lagoslakeid", llid), 4326))
@@ -22,11 +23,20 @@ get_buffer_stats <- function(llid){
   network <- suppressWarnings(suppressMessages(extract_network(lon = ll_pnt[1], 
                                                                lat = ll_pnt[2], 
                                                                maxsteps = Inf)))
-  network <- network[unlist(lapply(
-    st_intersects(network, st_transform(ll_lake, st_crs(network))), 
-    function(x) length(x) == 0)),]
+  if(!is.na(network)){
+    network <- network[unlist(lapply(
+      st_intersects(network, st_transform(ll_lake, st_crs(network))), 
+      function(x) length(x) == 0)),]
+    has_streams <- TRUE
+  }else{
+    network     <- ll_lake
+    has_streams <- FALSE
+  }
   
-  network_buffer <- st_buffer(network, 100)
+  network_buffer <- st_buffer(
+    st_transform(network, projection(nlcd)), 
+    units::as_units(100, "m"))
+  network_buffer <- st_transform(network_buffer, 4326)
   nlcd           <- get_nlcd(template = as_Spatial(st_as_sfc(
     st_bbox(network_buffer))), label = llid)
   network_buffer <- st_transform(network_buffer, projection(nlcd))
@@ -37,34 +47,38 @@ get_buffer_stats <- function(llid){
   names(nlcd_df)[3] <- "value"
   cols              <- dplyr::filter(pal_nlcd(), code %in% unique(nlcd_df[,3]))
   
-  # g_map <- ggplot() + 
-  #   geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
-  #   scale_fill_manual(values = cols$color) +
-  #   geom_sf(data = network_buffer) +
-  #   coord_sf(datum = NULL) +
-  #   theme(legend.position = "none", 
-  #         axis.text = element_blank(), 
-  #         axis.title = element_blank(), 
-  #         line = element_blank(), 
-  #         rect = element_blank(), 
-  #         text = element_blank(), 
-  #         panel.grid = element_blank())
+  g_map <- ggplot() +
+    geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
+    scale_fill_manual(values = cols$color) +
+    geom_sf(data = network_buffer) +
+    coord_sf(datum = NULL) +
+    theme(legend.position = "none",
+          axis.text = element_blank(),
+          axis.title = element_blank(),
+          line = element_blank(),
+          rect = element_blank(),
+          text = element_blank(),
+          panel.grid = element_blank())
   
   buffer_stats <- join_key(as.character(
     unlist(raster::extract(nlcd, network_buffer)))) %>%
     mutate(llid = llid)
-  nlcd_stats <- join_key(as.character(values(nlcd)))
+  nlcd_stats             <- join_key(as.character(values(nlcd)))
+  nlcd_stats$has_streams <- has_streams
+  
+  list(nlcd_stats = nlcd_stats, g_map = g_map)
 }
 
 pb <- progress_bar$new(format = "  pulling buffer lulc for :llid [:bar]", 
                        total = nrow(ep), 
                        clear = FALSE)
 res <- list()
-for(i in seq_along(ep$lagoslakeid[1:3])){
-  # llid <- ep$lagoslakeid[2]
-  llid <- ep$lagoslakeid[i]
+for(i in seq_along(ep$lagoslakeid[1:10])){
+  # llid <- ep$lagoslakeid[5]
+  llid          <- ep$lagoslakeid[i]
   pb$tick(tokens = list(llid = llid))
-  res[[i]] <- get_buffer_stats(llid)
+  res[[i]]      <- get_buffer_stats(llid)$nlcd_stats
+  res[[i]]$llid <- llid
 }
 
 buffer_lulc <- suppressWarnings(dplyr::bind_rows(res))
