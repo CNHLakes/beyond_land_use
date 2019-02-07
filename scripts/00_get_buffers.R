42fc81a8c3e5b957591915bf03058bcb76b9a270
jsta <stachel2@msu.edu>
2019-02-07 17:41:35 -0500

    pull lake buffers as well as stream buffers, track area, closes #3

--- scripts/00_get_buffers.R
+++ scripts/00_get_buffers.R
@@ -18,29 +18,46 @@ get_buffer_stats <- function(llid){
   ll_pnt <- st_coordinates(
     st_transform(
       query_gis("LAGOS_NE_All_Lakes_4ha_POINTS", "lagoslakeid", llid), 4326))
+  
+  # pull lake buffer
   ll_lake <- st_transform(
     query_gis("LAGOS_NE_All_Lakes_4ha", "lagoslakeid", llid), 4326)
+  lake_buffer      <- st_buffer(
+    st_transform(ll_lake, nhdR:::albers_conic()), 
+    units::as_units(100, "m"))
+  lake_buffer      <- st_difference(lake_buffer, st_transform(
+    fill_holes(ll_lake, 10000), nhdR:::albers_conic()))
+  lake_buffer_area <- sum(st_area(lake_buffer))
+  
+  # pull stream network buffer
   network <- suppressWarnings(suppressMessages(extract_network(lon = ll_pnt[1], 
                                                                lat = ll_pnt[2], 
                                                                maxsteps = Inf)))
-
   if(all(class(network) != "logical")){
     network <- network[unlist(lapply(
       st_intersects(network, st_transform(ll_lake, st_crs(network))), 
       function(x) length(x) == 0)),]
     has_streams <- TRUE
+    stream_length <- sum(st_length(network))
+    
+    network_buffer <- st_buffer(
+      st_transform(network, nhdR:::albers_conic()), 
+      units::as_units(100, "m"))
+    stream_buffer_area <- sum(st_area(network_buffer))
   }else{
-    network     <- ll_lake
-    has_streams <- FALSE
+    has_streams   <- FALSE
+    stream_length <- 0
+    stream_buffer_area   <- 0
   }
   
-  network_buffer <- st_buffer(
-    st_transform(network, nhdR:::albers_conic()), 
-    units::as_units(100, "m"))
-  network_buffer <- st_transform(network_buffer, 4326)
-  nlcd           <- get_nlcd(template = as_Spatial(st_as_sfc(
-    st_bbox(network_buffer))), label = llid)
-  network_buffer <- st_transform(network_buffer, projection(nlcd))
+  # pull nlcd
+  buffer_aoi <- st_union(st_as_sfc(
+    st_bbox(network_buffer)), 
+    st_as_sfc(
+      st_bbox(lake_buffer)))
+  buffer_aoi <- st_transform(buffer_aoi, 4326)
+  
+  nlcd           <- get_nlcd(template = as_Spatial(buffer_aoi), label = llid)
   
   nlcd_df           <- as.data.frame(nlcd, xy = TRUE) %>% 
     dplyr::select(x, y, contains("Value"))
@@ -48,6 +65,7 @@ get_buffer_stats <- function(llid){
   names(nlcd_df)[3] <- "value"
   cols              <- dplyr::filter(pal_nlcd(), code %in% unique(nlcd_df[,3]))
   
+  # construct plot for debugging
   g_map <- ggplot() +
     geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
     scale_fill_manual(values = cols$color) +
@@ -62,19 +80,35 @@ get_buffer_stats <- function(llid){
           panel.grid = element_blank())
   
   beginCluster()
-  buffer_stats <- join_key(as.character(
-    unlist(raster::extract(nlcd, network_buffer)))) %>%
+  # pull stream buffer nlcd
+  if(has_streams){
+    stream_buffer_stats <- join_key(as.character(
+      unlist(raster::extract(nlcd, network_buffer)))) %>%
+      mutate(llid = llid)
+  }else{
+    stream_buffer_stats <- NA
+  }
+  
+  # pull lake buffer nlcd
+  lake_buffer_stats <- join_key(as.character(
+    unlist(raster::extract(nlcd, lake_buffer)))) %>%
     mutate(llid = llid)
   endCluster()
   
+  # save bbox nlcd
   nlcd_stats             <- join_key(as.character(values(nlcd)))
-  nlcd_stats$has_streams <- has_streams
   
-  list(nlcd_stats = nlcd_stats, g_map = g_map)
+  list(nlcd_stats = nlcd_stats, 
+       stream_buffer_stats = stream_buffer_stats, 
+       stream_length = stream_length,
+       stream_buffer_area = stream_buffer_area,
+       lake_buffer_stats = lake_buffer_stats, 
+       lake_buffer_area = lake_buffer_area,
+       g_map = g_map)
 }
 
-# llid <- ep$lagoslakeid[10]
-# get_buffer_stats(llid)$nlcd_stats
+# llid <- ep$lagoslakeid[1]
+# test <- get_buffer_stats(llid)
 
 pb <- progress_bar$new(format = "  pulling buffer lulc for :llid [:bar]", 
                        total = nrow(ep), 
