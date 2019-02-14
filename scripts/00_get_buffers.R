c19f476795cbac9e431b3bb895ae76b49e90adcb
jsta <stachel2@msu.edu>
2019-02-14 11:32:07 -0500

    speed up extraction operations for large watersheds

--- scripts/00_get_buffers.R
+++ scripts/00_get_buffers.R
@@ -14,7 +14,9 @@ join_key <- function(vals){
 }
 
 get_buffer_stats <- function(llid){
-  # llid <- 2167
+  # llid <- 2763
+  # LAGOSNE::lake_info(2763)
+  # llid <- ep$lagoslakeid[1]
   ll_pnt <- st_coordinates(
     st_transform(
       query_gis("LAGOS_NE_All_Lakes_4ha_POINTS", "lagoslakeid", llid), 4326))
@@ -72,46 +74,47 @@ get_buffer_stats <- function(llid){
   # pull nlcd
   nlcd              <- get_nlcd(template = as_Spatial(buffer_aoi), 
                                 label = llid)
-  nlcd_df           <- as.data.frame(nlcd, xy = TRUE) %>% 
-    dplyr::select(x, y, contains("Value"))
-  nlcd_df[,3]       <- factor(nlcd_df[,3])
-  names(nlcd_df)[3] <- "value"
-  cols              <- dplyr::filter(pal_nlcd(), code %in% unique(nlcd_df[,3]))
   
   # construct plot for debugging
-  if(all(class(network) != "logical")){
-    g_map <- ggplot() +
-      geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
-      scale_fill_manual(values = cols$color) +
-      geom_sf(data = network_buffer) +
-      coord_sf(datum = NULL) +
-      theme(legend.position = "none",
-            axis.text = element_blank(),
-            axis.title = element_blank(),
-            line = element_blank(),
-            rect = element_blank(),
-            text = element_blank(),
-            panel.grid = element_blank())
-  }else{
-    g_map <- ggplot() +
-      geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
-      scale_fill_manual(values = cols$color) +
-      geom_sf(data = lake_buffer) +
-      coord_sf(datum = NULL) +
-      theme(legend.position = "none",
-            axis.text = element_blank(),
-            axis.title = element_blank(),
-            line = element_blank(),
-            rect = element_blank(),
-            text = element_blank(),
-            panel.grid = element_blank())
-  }
+  # nlcd_df           <- as.data.frame(nlcd, xy = TRUE) %>% 
+  #   dplyr::select(x, y, contains("Value"))
+  # nlcd_df[,3]       <- factor(nlcd_df[,3])
+  # names(nlcd_df)[3] <- "value"
+  # cols              <- dplyr::filter(pal_nlcd(), code %in% unique(nlcd_df[,3]))
+  # 
+  # if(all(class(network) != "logical")){
+  #   g_map <- ggplot() +
+  #     geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
+  #     scale_fill_manual(values = cols$color) +
+  #     geom_sf(data = network_buffer) +
+  #     coord_sf(datum = NULL) +
+  #     theme(legend.position = "none",
+  #           axis.text = element_blank(),
+  #           axis.title = element_blank(),
+  #           line = element_blank(),
+  #           rect = element_blank(),
+  #           text = element_blank(),
+  #           panel.grid = element_blank())
+  # }else{
+  #   g_map <- ggplot() +
+  #     geom_raster(data = nlcd_df, aes(x = x, y = y, fill = value)) +
+  #     scale_fill_manual(values = cols$color) +
+  #     geom_sf(data = lake_buffer) +
+  #     coord_sf(datum = NULL) +
+  #     theme(legend.position = "none",
+  #           axis.text = element_blank(),
+  #           axis.title = element_blank(),
+  #           line = element_blank(),
+  #           rect = element_blank(),
+  #           text = element_blank(),
+  #           panel.grid = element_blank())
+  # }
   
   suppressMessages(beginCluster())
   # pull stream buffer nlcd
   if(has_streams){
-    suppressMessages(stream_buffer_stats <- join_key(as.character(
-      unlist(raster::extract(nlcd, network_buffer)))) %>%
+    suppressMessages(stream_buffer_stats <- join_key(
+      as.character(na.omit(values(raster::mask(nlcd, network_buffer))))) %>%
       mutate(llid = llid))
   }else{
     stream_buffer_stats <- setNames(data.frame(NA, NA, NA, NA, NA, llid), 
@@ -120,9 +123,9 @@ get_buffer_stats <- function(llid){
   }
   
   # pull lake buffer nlcd
-  lake_buffer_stats <- join_key(as.character(
-    unlist(raster::extract(nlcd, lake_buffer)))) %>%
-    mutate(llid = llid)
+  lake_buffer_stats <- join_key(
+    as.character(na.omit(values(raster::mask(nlcd, lake_buffer))))) %>%
+      mutate(llid = llid)
   endCluster()
   
   # save bbox nlcd
@@ -133,12 +136,11 @@ get_buffer_stats <- function(llid){
        stream_length = stream_length,
        stream_buffer_area = stream_buffer_area,
        lake_buffer_stats = lake_buffer_stats, 
-       lake_buffer_area = lake_buffer_area,
-       g_map = g_map)
+       lake_buffer_area = lake_buffer_area)
 }
 
 # llid <- ep$lagoslakeid[5]
-# llid <- 6874
+# llid <- 2763
 # test <- get_buffer_stats(llid)
 
 pb <- progress_bar$new(format = "  pulling buffer lulc for :llid [:bar]", 
