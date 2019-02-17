eb67080804d733d6927b385d38b7fbaf2c2ba371
jsta <stachel2@msu.edu>
2019-02-17 15:51:45 -0500

    towards verifying buffer calcs

--- scripts/01_process_buffers.R
+++ scripts/01_process_buffers.R
@@ -2,11 +2,14 @@ library(assertr)
 library(purrr)
 library(readr)
 library(dplyr)
+library(LAGOSNE)
 
 ep                     <- readRDS("data/ep.rds")
 buffer_metadata_folder <- "data/buffer_lulc/"
-stream_lulc            <- "data/stream_buffer_stats.csv"
-lake_lulc              <- "data/lake_buffer_stats.csv"
+stream_lulc            <- read.csv("data/stream_buffer_stats.csv", 
+                                   stringsAsFactors = FALSE)
+lake_lulc              <- read.csv("data/lake_buffer_stats.csv", 
+                                   stringsAsFactors = FALSE)
 
 # read.csv(list.files(buffer_metadata_folder, full.names = TRUE)[14])
 
@@ -20,6 +23,27 @@ buffer_metadata <- buffer_metadata_folder %>%
   verify(all(ep$lagoslakeid %in% llid)) %>%
   verify(!any(is.na(as.numeric(stream_length))))
 
-buffer_summary <- function(){
+buffer_summary <- function(x){
+  unique(lake_lulc$description)
+  unique(stream_lulc$description)
+  
+  # x <- lake_lulc
+  test <- x %>%
+    dplyr::filter(llid != "llid") %>%
+    distinct(llid, description, .keep_all = TRUE)
+  
+  test2 <- group_by(test, llid) %>%
+    summarize(total_n = sum(as.numeric(n))) %>%
+    arrange(desc(total_n))
+  
+  lake_info(4717)
+  
+  test3 <- left_join(test2, buffer_metadata)
+  
+  test2[1,]$total_n * 30
+  
+  dplyr::filter(buffer_metadata, llid == test2[1,]$llid)
+  
   
 }
+
