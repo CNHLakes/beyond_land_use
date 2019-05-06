22aef3abc81cc096985dcd657fbb4bf2b82f6203
jsta <stachel2@msu.edu>
2019-05-06 11:41:21 -0400

    fix cdl processing dplyr errors

--- scripts/01_process_cdl.R
+++ scripts/01_process_cdl.R
@@ -14,19 +14,20 @@ cdl_summary <- function(llid){
   # llid <- 4393
   r <- raster(r_list[grep(paste0(llid, ".tif"), r_list)[1]])
   
-  cdl_table <- as_data_frame(table(values(r)), stringsAsFactors = FALSE) %>%
+  cdl_table <- as.data.frame(table(values(r)), stringsAsFactors = FALSE) %>%
     mutate(code = as.integer(Var1)) %>%
     dplyr::select(-Var1) %>%
     left_join(cdl_key, by = "code") 
   
   cdl_cat <- cdl_table %>%
-    filter(description != "Background") %>%
+    dplyr::filter(description != "Background") %>%
+    rename(n = Freq) %>%
     arrange(desc(n)) %>%
     group_by(category) %>%
     summarize(group_n = sum(n, na.rm = TRUE), is_ag = first(is_ag)) %>%
     ungroup() %>%
     mutate(percent = round((group_n / sum(group_n, na.rm = TRUE)) * 100, 0)) %>%
-    filter(percent >= 1) %>%
+    dplyr::filter(percent >= 1) %>%
     arrange(is_ag, percent)
   cdl_cat <- add_row(cdl_cat, category = "other", group_n = NA,
                       percent = 100 - sum(cdl_cat$percent, na.rm = TRUE))
@@ -34,6 +35,7 @@ cdl_summary <- function(llid){
     
   cdl_isag <- cdl_table %>%
     filter(description != "Background") %>%
+    rename(n = Freq) %>%
     arrange(desc(n)) %>%
     group_by(is_ag) %>%
     summarize(group_n = sum(n, na.rm = TRUE)) %>%
@@ -43,6 +45,7 @@ cdl_summary <- function(llid){
   
   cdl_isforage <- cdl_table %>%
     filter(description != "Background" & !is.na(description)) %>%
+    rename(n = Freq) %>%
     arrange(desc(n)) %>%
     group_by(is_forage) %>%
     summarize(group_n = sum(n, na.rm = TRUE)) %>%
@@ -52,12 +55,14 @@ cdl_summary <- function(llid){
     arrange(percent)
   
   res <- mutate(cdl_cat, llid = llid, variable = category, value = percent) %>%
-    select(llid, variable, value) %>%
+    dplyr::select(llid, variable, value) %>%
     rbind(
-      select(mutate(cdl_isag, llid = llid, variable = is_ag, value = percent), 
-             llid, variable, value),
-      select(mutate(cdl_isforage, llid = llid, variable = is_forage, value = percent), 
-             llid, variable, value)
+      dplyr::select(mutate(cdl_isag, llid = llid, variable = is_ag, 
+                           value = percent), 
+                    llid, variable, value),
+      dplyr::select(mutate(cdl_isforage, llid = llid, variable = is_forage, 
+                           value = percent), 
+                    llid, variable, value)
       )
   
   res[!duplicated(res$variable),]
