a990a8c918640bef123a98bd1053461500616006
jsta <stachel2@msu.edu>
2022-06-03 13:22:36 -0600

    stash histogram fig

---
+++ figures/tile_drainage_histogram.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(ggplot2)
+library(sf)
+library(cowplot)
+
+signif_hu4_ids <- c("HU4_56", "HU4_57")
+dt <- readRDS("../data/dt.rds") %>%
+  mutate(signif_hu4 = hu4_zoneid %in% signif_hu4_ids) %>%
+  dplyr::select(lagoslakeid, hu4_zoneid, clay_pct, signif_hu4) %>%
+  group_by(hu4_zoneid) %>%
+  mutate(median = median(clay_pct, na.rm = TRUE)) %>%
+  distinct(hu4_zoneid, median, signif_hu4)
+
+test  <- readRDS("../data/tile_drainage.rds")
+test2 <- left_join(test, dt, by = c("ZoneID" = "hu4_zoneid"))
+
+# scatter plot clay versus tile drainage highlighting sensitve hucs
+gg_hist <- ggplot(data = test2, aes(tile_drainage/900 * 100 # grid cells are 30x30 = 900
+    )) + 
+  geom_histogram(binwidth=5, aes(fill=signif_hu4)) +  
+  scale_fill_manual(values = c("FALSE" = "grey", "TRUE" = "red")) +
+  xlab("Tile drainage (percent)") + ylab("Count (n)") +
+  cowplot::theme_cowplot() +
+  theme(legend.position = "none", 
+        legend.background = element_rect(fill = "white", color = "black"),
+        legend.text = element_text(size = 10), 
+        legend.title = element_text(size = 11)) +
+  labs(color = "LULC Sensitive")
+
+ggsave("test.png",gg_hist, height=3, width=3.3)
+ 
