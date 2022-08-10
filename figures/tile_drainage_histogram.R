8af34fe492cbb75a6d147cf3f62e997bce94811e
jsta <stachel2@msu.edu>
2022-08-09 21:10:48 -0600

    linting, fig size

--- figures/tile_drainage_histogram.R
+++ figures/tile_drainage_histogram.R
@@ -15,17 +15,16 @@ test  <- readRDS("../data/tile_drainage.rds")
 test2 <- left_join(test, dt, by = c("ZoneID" = "hu4_zoneid"))
 
 # scatter plot clay versus tile drainage highlighting sensitve hucs
-gg_hist <- ggplot(data = test2, aes(tile_drainage/900 * 100 # grid cells are 30x30 = 900
-    )) + 
-  geom_histogram(binwidth=5, aes(fill=signif_hu4), size=0.5, color="black") +  
+gg_hist <- ggplot(data = test2, aes(tile_drainage / 900 * 100 # grid cells are 30x30 = 900
+)) +
+  geom_histogram(binwidth = 5, aes(fill = signif_hu4), size = 0.5, color = "black") +
   scale_fill_manual(values = c("FALSE" = "white", "TRUE" = "red")) +
   xlab("Tile drainage (percent)") + ylab("Count (n)") +
   cowplot::theme_cowplot() +
-  theme(legend.position = "none", 
-        legend.background = element_rect(fill = "white", color = "black"),
-        legend.text = element_text(size = 10), 
-        legend.title = element_text(size = 11)) +
+  theme(legend.position = "none",
+    legend.background = element_rect(fill = "white", color = "black"),
+    legend.text = element_text(size = 10),
+    legend.title = element_text(size = 11)) +
   labs(color = "LULC Sensitive")
 
-ggsave("test.png",gg_hist, height=3, width=3.3)
- 
+ggsave("test.png", gg_hist, height = 2, width = 4.3)
